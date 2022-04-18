2a106a0673ed31b978b5ee2103ee2b66e9b4c359
SergeyMikhalev <tolltroll@yandex.ru>
2022-04-18 11:15:47 +0300

    Just test commit #2

--- src/main/java/ru/job4j/collection/TestInterface.java
+++ src/main/java/ru/job4j/collection/TestInterface.java
@@ -1,4 +1,10 @@
 package ru.job4j.collection;
 
 public interface TestInterface {
+    /**
+     * Проверка синхронизации проектов на разных рабочих местах через
+     * GitHub
+     *
+     * */
+
 }

8242d8de0dfe7104ef17a93d7b12d0d0232ae4ad
Сергей <tolltroll@yandex.ru>
2022-04-27 20:13:14 +0300

    SimpleTree #3

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,8 @@
 package ru.job4j.tree;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;

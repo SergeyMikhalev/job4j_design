1ded9de7a740e66bdb661d22a12be28bc8f32f5f
SergeyMikhalev <tolltroll@yandex.ru>
2022-04-28 14:04:25 +0300

    SimpleQueue #5

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,20 +5,20 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-
-        while (!in.isEmpty()) {
-            out.push(in.pop());
-        }
+        checkAndReplace();
         return out.pop();
 
     }
 
     public void push(T value) {
-        while (!out.isEmpty()) {
-            in.push(out.pop());
-        }
-
         in.push(value);
+    }
 
+    private void checkAndReplace() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
     }
 }

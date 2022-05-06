7813d24d0f4e3a72a3f523749eaeb0556ea9ed4c
Сергей <tolltroll@yandex.ru>
2022-05-06 23:40:06 +0300

    Log filter

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,9 +2,8 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.AbstractList;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class LogFilter {
@@ -16,7 +15,7 @@ public class LogFilter {
                 String[] words = s.split(" ");
                 return words[words.length - 2].equals("404");
             }).forEach(rsl::add);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return rsl;
@@ -25,7 +24,7 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
 
     }
 }

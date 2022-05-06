7ea649727c7c96e1200cb1d47b6b96ec3fd56839
SergeyMikhalev <tolltroll@yandex.ru>
2022-05-06 14:59:31 +0300

    Even numbers file

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+
+            System.out.println("Numbers from even.txt");
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.valueOf(line) % 2 == 0) {
+                    System.out.println(line + " is even");
+                } else {
+                    System.out.println(line + " is not even");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

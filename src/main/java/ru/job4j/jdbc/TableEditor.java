9f9a0f19a491a88bebb08390100a87de60228c10
Сергей <tolltroll@yandex.ru>
2022-06-11 17:30:49 +0300

    Table editor

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,7 +1,6 @@
 package ru.job4j.jdbc;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 import java.util.StringJoiner;
@@ -24,38 +23,47 @@ public class TableEditor implements AutoCloseable {
             String url = properties.getProperty("hibernate.connection.url");
             String login = properties.getProperty("hibernate.connection.username");
             String password = properties.getProperty("hibernate.connection.password");
-            try (Connection con = DriverManager.getConnection(url, login, password)) {
-                DatabaseMetaData metaData = con.getMetaData();
-                System.out.println(metaData.getUserName());
-                System.out.println(metaData.getURL());
-                connection = con;
-            }
+
+
+            connection = DriverManager.getConnection(url, login, password);
+
         } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void executeSQL(String sql) {
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void createTable(String tableName) {
         final String sql = String.format("create table if not exists %s() ;", tableName);
-        System.out.println(sql);
-
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        executeSQL(sql);
     }
 
     public void dropTable(String tableName) {
+        final String sql = String.format("drop table if exists %s;", tableName);
+        executeSQL(sql);
     }
 
     public void addColumn(String tableName, String columnName, String type) {
+        final String sql = String.format("alter table if exists %s add column if not exists %s %s ;", tableName, columnName, type);
+        executeSQL(sql);
     }
 
     public void dropColumn(String tableName, String columnName) {
+        final String sql = String.format("alter table if exists %s drop column if exists %s ;", tableName, columnName);
+        executeSQL(sql);
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
+        final String sql = String.format("alter table if exists %s rename column %s to %s ;", tableName, columnName, newColumnName);
+        executeSQL(sql);
     }
 
 
@@ -87,13 +95,36 @@ public class TableEditor implements AutoCloseable {
 
     public static void main(String... args) {
         Properties properties = new Properties();
+        final String table_name = "example_table";
+
         try (FileReader fileReader = new FileReader("app.properties")) {
             properties.load(fileReader);
             TableEditor tableEditor = new TableEditor(properties);
 
-            tableEditor.createTable("ExampleTable");
-            TableEditor.getTableScheme(tableEditor.connection, "ExampleTable");
+            System.out.println("--Create--");
+            tableEditor.createTable(table_name);
+            System.out.println(TableEditor.getTableScheme(tableEditor.connection, table_name));
+            System.out.println();
 
+            System.out.println("--Add column--");
+            tableEditor.addColumn(table_name, "column1", "integer");
+            tableEditor.addColumn(table_name, "column2", "varchar(255)");
+            System.out.println(TableEditor.getTableScheme(tableEditor.connection, table_name));
+            System.out.println();
+
+            System.out.println("--Rename column--");
+            tableEditor.renameColumn(table_name, "column1", "co3");
+            System.out.println(TableEditor.getTableScheme(tableEditor.connection, table_name));
+            System.out.println();
+
+
+            System.out.println("--Drop column--");
+            tableEditor.dropColumn(table_name, "column2");
+            System.out.println(TableEditor.getTableScheme(tableEditor.connection, table_name));
+            System.out.println();
+
+            System.out.println("--Drop table--");
+            tableEditor.dropTable(table_name);
 
         } catch (Exception e) {
             e.printStackTrace();

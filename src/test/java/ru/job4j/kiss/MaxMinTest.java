2e59448ac87c03cb6586e9c988fb7344fe923cce
SergeyMikhalev <tolltroll@yandex.ru>
2022-06-30 15:02:54 +0300

    MaxMin

--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -9,38 +9,38 @@ import java.util.List;
 
 public class MaxMinTest {
     @Test(expected = IllegalArgumentException.class)
-    public void WhenEmptyList() {
+    public void whenEmptyList() {
         new MaxMin().max(new ArrayList<Integer>(), Comparator.naturalOrder());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void WhenListIsNull() {
+    public void whenListIsNull() {
         List<Integer> list = null;
         new MaxMin().max(list, Comparator.naturalOrder());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void WhenComparatorIsNull() {
+    public void whenComparatorIsNull() {
         new MaxMin().max(List.of(1, 2, 3, 4), null);
     }
 
     @Test
-    public void WhenMaxOfOne() {
+    public void whenMaxOfOne() {
         Assert.assertEquals((long) (new MaxMin().max(List.of(77), Comparator.naturalOrder())), 77L);
     }
 
     @Test
-    public void WhenMaxOfMany() {
+    public void whenMaxOfMany() {
         Assert.assertEquals((long) (new MaxMin().max(List.of(0, 12, 6, 9, 77), Comparator.naturalOrder())), 77L);
     }
 
     @Test
-    public void WhenMinOfOne() {
+    public void whenMinOfOne() {
         Assert.assertEquals((long) (new MaxMin().min(List.of(77), Comparator.naturalOrder())), 77L);
     }
 
     @Test
-    public void WhenMinOfMany() {
+    public void whenMinOfMany() {
         Assert.assertEquals((long) (new MaxMin().min(List.of(88, 12, 6, 9, 77), Comparator.naturalOrder())), 6L);
     }
 }

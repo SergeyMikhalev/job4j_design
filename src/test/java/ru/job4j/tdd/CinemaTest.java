5cd450f5ab461cac188827be231f05cc9329d248
SergeyMikhalev <tolltroll@yandex.ru>
2022-07-04 09:52:49 +0300

    TDD 2

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,15 +1,17 @@
 package ru.job4j.tdd;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore("Classes not implemented yet")
 public class CinemaTest {
 
     @Test
@@ -39,4 +41,40 @@ public class CinemaTest {
         Assert.assertEquals(sessions.size(), 0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongRow() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, -1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongColumn() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, -1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1919, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBoughtTwice() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket1 = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+    }
 }

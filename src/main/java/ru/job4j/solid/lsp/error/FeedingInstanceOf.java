3794fb163d9c8e535f9c4bf47d78610d30ed5ab3
SergeyMikhalev <tolltroll@yandex.ru>
2022-07-18 18:37:57 +0300

    LSP

---
+++ src/main/java/ru/job4j/solid/lsp/error/FeedingInstanceOf.java
@@ -0,0 +1,28 @@
+package ru.job4j.solid.lsp.error;
+
+public class FeedingInstanceOf {
+    /*
+    * Использование инстанс оф или гет класс в таком духе - плохая практика.
+    * Правильнее использовать перегруженный метод класса.
+    * */
+
+    public void feed(Animal animal) {
+        if (animal instanceof Cow) {
+            System.out.println("Eating grass");
+        }
+
+        if (animal instanceof Wolf) {
+            System.out.println("Eating grass");
+        }
+    }
+
+}
+
+class Animal {
+}
+
+class Cow extends Animal {
+}
+
+class Wolf extends Animal {
+}

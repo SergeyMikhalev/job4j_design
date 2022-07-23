aee06c7da4fd7de6e33244d60bc807103c6da5d4
Сергей <tolltroll@yandex.ru>
2022-07-23 17:52:06 +0300

    ISP Error

--- src/main/java/ru/job4j/solid/isp/error/ParkingLot.java
+++ src/main/java/ru/job4j/solid/isp/error/ParkingLot.java
@@ -1,16 +1,16 @@
 package ru.job4j.solid.isp.error;
 
 public interface ParkingLot {
-    void parkCar();  // Decrease empty spot count by 1
+    void parkCar();
 
-    void unparkCar(); // Increase empty spots by 1
+    void unparkCar();
 
-    void getCapacity();  // Returns car capacity
+    void getCapacity();
 
     /* Последние два метода окажутся лишними, если мы предположим, что бывают ещё
      * и бесплатные парковки. Их стоит отделить в другой интерфейс.
      */
-    double calculateFee(Car car); // Returns the price based on number of hours
+    double calculateFee(Car car);
 
     void doPayment(Car car);
 }

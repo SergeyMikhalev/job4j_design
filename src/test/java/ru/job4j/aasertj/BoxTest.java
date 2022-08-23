cfa91e097711df034c1334cf4cc79235faf55759
Сергей <tolltroll@yandex.ru>
2022-08-23 20:42:57 +0300

    Tests 2

---
+++ src/test/java/ru/job4j/aasertj/BoxTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.aasertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 12);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void sphereVertexes() {
+        Box box = new Box(0, 10);
+        Integer vertexes = box.getNumberOfVertices();
+        assertThat(vertexes).isZero();
+    }
+
+    @Test
+    void cubeVertexes() {
+        Box box = new Box(8, 12);
+        Integer vertexes = box.getNumberOfVertices();
+        assertThat(vertexes).isEqualTo(8);
+    }
+
+    @Test
+    void isExist() {
+        Box box = new Box(8, 12);
+        Boolean exists = box.isExist();
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    void isNotExist() {
+        Box box = new Box(169, 12);
+        Boolean exists = box.isExist();
+        assertThat(exists).isFalse();
+    }
+
+    @Test
+    void areaOfSphere() {
+        Box box = new Box(0, 10);
+        Double area = box.getArea();
+        assertThat(area).isEqualTo(1256.6370614359173);
+    }
+
+    @Test
+    void areaOfCube() {
+        Box box = new Box(8, 10);
+        Double area = box.getArea();
+        assertThat(area).isEqualTo(600.0);
+    }
+
+
+}

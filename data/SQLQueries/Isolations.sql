01abe7e4a7f4ed078ce66c61309e6de67579101d
Сергей <tolltroll@yandex.ru>
2022-08-31 16:39:22 +0300

    Isolation levels;

---
+++ data/SQLQueries/Isolations.sql
@@ -0,0 +1,17 @@
+create table heights (
+    id serial primary key,
+    name varchar(50),
+	height integer
+);
+
+insert into heights(name, height) values ('Vasia',155);
+insert into heights(name, height) values ('Kolia',170);
+insert into heights(name, height) values ('Petia',190);
+
+select * from heights;
+
+select AVG(height) from heights;
+
+update heights set height = 160 where name = 'Vasia';
+
+ update heights set height = 180 where name = 'Kolia';

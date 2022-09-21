df625a0fea2c2febc78f9a796357a2181c5c2fa9
Сергей <tolltroll@yandex.ru>
2022-09-21 17:26:38 +0300

    Searcher

--- src/main/java/ru/job4j/io/searcher/filters/MaskSearchFilter.java
+++ src/main/java/ru/job4j/io/searcher/filters/MaskSearchFilter.java
@@ -6,7 +6,8 @@ public class MaskSearchFilter implements SearchFilter {
     private final String regex;
 
     public MaskSearchFilter(String mask) {
-        this.regex = "^" +
+        this.regex = "^"
+                +
                 mask
                         .replace(".", "[.]")
                         .replace("*", ".*")
